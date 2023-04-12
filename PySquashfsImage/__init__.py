786e3a21170d6b83c5b436a363397080432221ca
AT0myks <at0myks.dev@gmail.com>
2023-04-12 05:13:05 +0200

    Styling, formatting and more (PEP 8 and 257)
    Make the code more readable and more compliant with the Python
    coding conventions (except line length).
    Most of the changes come from replacing tabs by spaces.

--- PySquashfsImage/__init__.py
+++ PySquashfsImage/__init__.py
@@ -2,4 +2,4 @@ from .PySquashfsImage import SquashFsImage
 from .PySquashfsImage import SquashedFile
 from .PySquashfsImage import SquashInode
 
-__all__ = ['SquashFsImage','SquashedFile','SquashInode']
+__all__ = ['SquashFsImage', 'SquashedFile', 'SquashInode']
