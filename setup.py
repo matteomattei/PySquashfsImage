35ca35d841e2a2c89f4a4631dec49f69ea9bd0d3
AT0myks <at0myks.dev@gmail.com>
2023-07-01 17:53:54 +0200

    Update classifiers

--- setup.py
+++ setup.py
@@ -44,7 +44,10 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Filesystems",
+        "Topic :: Utilities",
     ],
     entry_points={
         "console_scripts": [
